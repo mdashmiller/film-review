d41146270a93d95e3558a5f971d3ea1395688163
mdashmiller <m.robert.miller@gmail.com>
2019-07-12 10:45:11 -0500

    add movie cards with static data

---
+++ src/components/MovieCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MovieCard = ({ title, year, description, imageURL }) => (
+  <div className="card">
+    <img className="card-img-top" src={imageURL} alt="Card cap" />
+    <div className="card-body">
+      <h5 className="card-title">{title}</h5>
+      <p className="card-text">{description}</p>
+      <p className="card-text">
+        <small className="text-muted">{year}</small>
+      </p>
+    </div>
+  </div>
+)
+
+export default MovieCard
