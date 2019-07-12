0375d91715be758fcf4f1b6e638767104bdd841d
mdashmiller <m.robert.miller@gmail.com>
2019-07-12 10:22:28 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// Import Bootstrap CSS and JS
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
